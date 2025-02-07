5c8408aec208d5200add83a9ee28785c497e40fa
tommasoAlteri98 <127266977+tommasoAlteri98@users.noreply.github.com>
2025-02-07 10:51:40 +0100

    commit es1 ven7febb concluso

--- esercito.py
+++ esercito.py
@@ -16,7 +16,7 @@ class Artiglieria(UnitaMilitare):
     def __init__(self, nome, numero_soldati, numero_cannoni):
         super().__init__(nome, numero_soldati)
         self.numero_cannoni = numero_cannoni
-
+ 
     def calibra_artiglieria(self):
         print("L'unità " + self.nome + " sta calibrando l'artiglieria.")
 
@@ -57,4 +57,38 @@ class ControlloMilitare:
         self.unitaMilitari.append(unita)
         print("L'unità " + unita.nome + " è stata aggiunta alla lista.")
 
+    def mostra_unita(self):
+        if self.unitaMilitari == True:
+            print("Unità registrate:")
+            for unita in self.unitaMilitari:
+                print("Nome: " + unita.nome +", Numero soldati: " + str(unita.numero_soldati))
+        else:
+            print("Non ci unità militari registrate.")
+
+    def dettagli_unita(self, nome):
+        for unita in self.unitaMilitari:
+            if unita.nome == nome:
+                print("Unità " + unita.nome +":")
+                print("Numero soldati: " + str(unita.numero_soldati))
+                return
+        print("Unità con il nome " + nome + " non trovata.")
+        
     
+fanteria = Fanteria("Fanteria Gruppo 1", 150)
+artiglieria = Artiglieria("Artiglieria Itachi", 40, 100)
+cavalleria = Cavalleria("Cavalleria Loky", 30, 50)
+supporto = SupportoLogistico("Supporto Logistico", 20)
+ricognizione = Ricognizione("Ricognizione Rosso", 15)
+
+controlloReparti = ControlloMilitare()
+
+controlloReparti.registra_unita(fanteria)
+controlloReparti.registra_unita(artiglieria)
+controlloReparti.registra_unita(cavalleria)
+controlloReparti.registra_unita(supporto)
+controlloReparti.registra_unita(ricognizione)
+
+controlloReparti.mostra_unita()
+
+controlloReparti.dettagli_unita("Artiglieria Itachi")
+controlloReparti.dettagli_unita("Cavalleria Loky")
